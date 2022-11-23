a13a02de2ce220d9da222c8c79d510dac7541e32
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-11-23 18:41:41 -0300

    Estruturas de repetição com EJS

--- modulo04/index.js
+++ modulo04/index.js
@@ -9,12 +9,22 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco: 5},
+        {nome: "Leite", preco: 1.45},
+        {nome: "Carne", preco: 15},
+        {nome: "Redbull", preco: 6},
+        {nome: "Nescau", preco: 4,5}
+    ]
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia",
         inscritos: 8000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 })
 
