722b4ff99eab7908d61595815cc03015ace1b568
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-11-23 18:10:52 -0300

    feat: Exibindo variáveis no HTML

--- modulo04/index.js
+++ modulo04/index.js
@@ -5,8 +5,15 @@ const ejs = require('ejs');
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia",
+        inscritos: 8000
+    });
 })
 
 app.listen(8080, () => {
