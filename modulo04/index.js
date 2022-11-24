53d1de4c2bb1d251d616f7a55826607822f58dcc
Antonio Cláudio Holanda Borges Junior <claudiohbjunior@gmail.com>
2022-11-23 23:40:28 -0300

    Apresentando e instalando o Bootstrap

--- modulo04/index.js
+++ modulo04/index.js
@@ -4,29 +4,12 @@ const ejs = require('ejs');
 
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
+app.use(express.static('public'))
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-cola", preco: 5},
-        {nome: "Leite", preco: 1.45},
-        {nome: "Carne", preco: 15},
-        {nome: "Redbull", preco: 6},
-        {nome: "Nescau", preco: 4,5}
-    ]
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+    res.render("index")
 })
 
-app.listen(8080, () => {
-    console.log("App rodando");});
+
+
+app.listen(8080, () => {console.log("App rodando");});
